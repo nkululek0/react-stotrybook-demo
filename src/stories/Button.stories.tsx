ccc286e664e946e4dfec9904496051e1a66caf94
nkululek0 <nkululekovuyo0103@gmail.com>
2025-09-29 17:08:47 +0200

    (Update): creates story for button

---
+++ src/stories/Button.stories.tsx
@@ -0,0 +1,40 @@
+import { Meta, StoryObj } from '@storybook/react';
+import { fn } from '@storybook/test';
+import { ComponentProps } from 'react';
+import { Button } from '../components/Button';
+
+type StoryProps = ComponentProps<typeof Button> & {
+  buttonText: string
+};
+
+const meta: Meta<StoryProps> = {
+  component: Button,
+  argTypes: {
+    variant: {
+      options: ['primary', 'secondary'],
+      control: { type: 'radio' }
+    },
+    size: {
+      options: ['sm', 'md', 'lg'],
+      control: { type: 'select' }
+    }
+  },
+  args: {
+    onClick: fn()
+  }
+};
+
+export default meta;
+
+type Story = StoryObj<StoryProps>;
+
+export const Primary: Story = {
+  args: {
+    buttonText: 'Click Me',
+    variant: 'primary',
+    size: 'md'
+  },
+  render({buttonText, ...args}) {
+    return <Button {...args}>{buttonText}</Button>;
+  }
+};
